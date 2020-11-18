26bda267e7805f39ae5d7ccc595ba95ff51564ef
ABourneron <antoine.bourneron@gmail.com>
2020-11-18 14:26:22 +0100

    First Thread with low priority

--- Lab5/main.c
+++ Lab5/main.c
@@ -10,13 +10,16 @@ void *first_thread(void *arg)
 {
     (void) arg;
     printf("Hello from thread %d\n", thread_getpid());
+    
+    while(1){}
 
     return NULL;
 }
 int main(void)
 {
+	//En mettant une prio plus forte, le thread est prioritaire face au main...
     thread_create(stack, sizeof(stack),
-                  THREAD_PRIORITY_MAIN - 1, flag,
+                  THREAD_PRIORITY_MAIN + 1, flag,
                   first_thread, NULL, "first_thread");
                   
     char line_buf[SHELL_DEFAULT_BUFSIZE];
